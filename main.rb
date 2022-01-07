d770b1d14fef7a52e3b98c14ee0c934659c9f230
ahmedadel56 <ahmdadl563@gmail.com>
2022-01-07 12:57:40 +0200

    Add fizzbuzz methode and reserve

--- main.rb
+++ main.rb
@@ -9,8 +9,21 @@ class Solver
       result
     end
   end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(num)
+    
+    if(num % 3).zero? && (num % 5).zero?
+      'fizzbuzz' 
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num
+    end
+  end
 end
-
-cal = Solver.new
-
-p cal.factorial(1)
